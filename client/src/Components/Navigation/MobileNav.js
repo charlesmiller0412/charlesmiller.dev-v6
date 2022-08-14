7493ad24fefb84f20c2e4b1b12bff79286c6b79f
Charles Miller <charles.miller.0412@gmail.com>
2022-08-14 16:14:44 -0500

    removed unnecessary code

--- client/src/Components/Navigation/MobileNav.js
+++ client/src/Components/Navigation/MobileNav.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import Images from "../../assets/Images";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
