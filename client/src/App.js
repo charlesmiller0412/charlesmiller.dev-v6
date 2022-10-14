d2056f0c58f5e85e893e8f7746443f92d0b48312
Charles Miller <charles.miller.0412@gmail.com>
2022-10-13 22:27:09 -0500

    removed loading screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import "./Sass/App.scss";
-import LoadingScreen from "./Components/Loading";
 import Home from "./Components/Home/Home";
 import About from "./Components/About";
 import Contact from "./Components/Contact/Contact";
@@ -12,27 +10,18 @@ import MobileNav from "./Components/Navigation/MobileNav";
 import Overlay from "./Components/Navigation/Overlay";
 
 export default function App() {
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() => setLoading(false), 3500);
-    });
     return (
         <>
-            {loading === false ? (
-                <main className="App">
-                    <Navbar />
-                    <Home />
-                    <Projects />
-                    <About />
-                    <Contact />
-                    <Footer />
-                    <MobileNav />
-                    <Overlay />
-                </main>
-            ) : (
-                <LoadingScreen />
-            )}
+            <main className="App">
+                <Navbar />
+                <Home />
+                <Projects />
+                <About />
+                <Contact />
+                <Footer />
+                <MobileNav />
+                <Overlay />
+            </main>
         </>
     );
 }
